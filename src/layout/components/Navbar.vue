5f445f2fbf1413aca30183b74016727cd268ce73
itheima <ithemia@itcast.cn>
2024-03-09 17:09:49 +0800

    完成修改密码的功能

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -24,7 +24,8 @@
           <a target="_blank" href="https://github.com/hfx521/houtaihr">
             <el-dropdown-item>项目地址</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
+          <!-- prevent阻止默认事件 -->
+          <a target="_blank" @click.prevent="updatePassword">
             <el-dropdown-item>修改密码</el-dropdown-item>
           </a>
           <!-- native事件修饰符 -->
@@ -35,6 +36,26 @@
         </el-dropdown-menu>
       </el-dropdown>
     </div>
+    <!-- 放置dialog -->
+    <!-- .sync可以接受子组件传过来的事件和值 -->
+    <el-dialog width="500px" title="修改密码" @close="btnCancel" :visible.sync="showDialog">
+      <!-- 放置表单 -->
+      <el-form ref="passForm" label-width="120px" :model="passForm" :rules="rules">
+        <el-form-item label="旧密码" prop="oldPassword">
+          <el-input v-model="passForm.oldPassword" show-password  size="small" />
+        </el-form-item>
+        <el-form-item label="新密码" prop="newPassword">
+          <el-input v-model="passForm.newPassword" show-password  size="small" />
+        </el-form-item>
+        <el-form-item label="重复密码" prop="confirmPassword">
+          <el-input v-model="passForm.confirmPassword" show-password size="small" />
+        </el-form-item>
+        <el-form-item>
+         <el-button @click="btnOK" size="mini" type="primary">确认修改</el-button>
+         <el-button @click="btnCancel" size="mini" type="primary">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -42,11 +63,45 @@
 import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
-
+// import { Footer as ElFooter } from 'element-ui'
+// 如果你想引入 element-ui 库的 Footer 组件，应该直接从 element-ui 中引入：
+import { updatePassword } from '@/api/user'
 export default {
   components: {
     Breadcrumb,
     Hamburger
+  },
+  data() {
+    return {
+      showDialog:false, //控制弹出层的显示和隐藏
+      passForm: {
+        oldPassword:'', //旧密码
+        newPassword:'', //新密码
+        confirmPassword:'' //确认新密码
+      },
+      rules: {
+        oldPassword:[{required: true, message:'旧密码不能为空', trigger:'blur'}], //旧密码
+        newPassword:[{required: true, message:'新密码不能为空',trigger:'blur'},{
+          trigger:'blur',
+          min:6,
+          max:16,
+          message:'新密码的长度为6-16位之间'
+        }], //新密码
+        confirmPassword:[{required: true, message:'重复密码不能为空', trigger:'blur'},{
+          trigger:'blur',
+          validator:(rule, value,callback) => {
+            //一定要使用箭头函数
+            //value
+           if(this.passForm.newPassword === value) {
+            callback()
+           }else{
+            callback(new Error('重复密码和新密码输入不一致'))
+           }
+          }
+        }] //确认新密码
+
+      }
+    }
   },
   computed: {
     //引入头像 和用户名称
@@ -57,6 +112,9 @@ export default {
     ])
   },
   methods: {
+    updatePassword () {
+      this.showDialog = true //显示弹出层
+    },
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar')
     },
@@ -64,6 +122,27 @@ export default {
       // 调用退出登陆的action
       await this.$store.dispatch('user/logout')
       this.$router.push('/login')
+    },
+    //确定
+    btnOK () {
+      this.$refs.passForm.validate(async isOK  => {
+        if (isOK) {
+          //调用接口
+          await updatePassword(this.passForm) 
+          this.$message.success('修改密码成功')
+          //成功了 成功之后重置表单
+          // this.$refs.passForm.resetFields() //重置表单
+          // //关闭弹层
+          // this.showDialog = false
+          btnCancel()
+        }
+      })
+    },
+    //取消
+    btnCancel() {
+      this.$refs.passForm.resetFields()//重置表单
+      //关闭弹层
+      this.showDialog = false
     }
   }
 }
