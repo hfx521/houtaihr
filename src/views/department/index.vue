738e1dd3d41c6a28a0d948ea4055a07c1f010797
itheima <ithemia@itcast.cn>
2024-03-10 19:54:16 +0800

    完成了添加用户的功能

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,54 +2,54 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree  :expand-on-click-node="false" :data="depts" :props="defaultProps" >
+      <el-tree :expand-on-click-node="false" default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
-        <!-- v-slot="{node,data}" 只能作用在template -->
-        <template v-slot="{data}">
-          <el-row style="width: 100%;;height: 40px;" type="flex" justify="space-between" align="middle">
-          <el-col>{{ data.name }}</el-col>
-          <el-col :span="4">
-            <span  style="margin-left: 10px" class="tree-manager">{{ data.managerName }}</span>
-            <!-- $event实参 表示类型 -->
-            <el-dropdown @command="operateDept($event,data.id)">
+        <!-- v-slot="{ node, data }" 只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <!-- $event 实参 表示类型 -->
+              <el-dropdown @command="operateDept($event, data.id)">
                 <!-- 显示区域内容 -->
-    <span class="el-dropdown-link">
-      操作<i class="el-icon-arrow-down el-icon--right"></i>
-    </span>
-    <!-- 下拉菜单选项 -->
-    <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item command="add">添加子部门</el-dropdown-item>
-      <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
-      <el-dropdown-item command="del">删除</el-dropdown-item>
-    </el-dropdown-menu>
-  </el-dropdown>
-          </el-col>
-        </el-row> 
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
         </template>
       </el-tree>
     </div>
-    <!-- 放置弹出层组件 -->
-    <!-- 表示会接受子组件的事件update:showDialog,值 => 属性 -->
-    <add-dept :current-node-id="currentNodeId" :showDialog.sync="showDialog" />
+    <!-- 放置弹层 -->
+    <!-- 表示会接受子组件的事件  update:showDialog, 值 => 属性 -->
+    <!-- ref 可以获取dom实例对象 ref也可以获取自定义组件的实例对象 -->
+    <add-dept ref="addDept" :current-node-id="currentNodeId" :show-dialog.sync="showDialog" @updateDepartment="getDepartment" />
   </div>
 </template>
-
 <script>
-import { getDepartment } from '@/api/department'
-import {transListToTreeData} from '@/utils'
+import { getDepartment, delDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils'
 import AddDept from './components/add-dept.vue'
 export default {
   name: 'Department',
-  components:{AddDept},
+  components: { AddDept },
   data() {
     return {
-      currentNodeId:null,//存储当前点击的id
-      showDialog:false, //控制弹层的显示和隐藏
-      depts: [], //数据属性
+      currentNodeId: null, // 存储当前点击的id
+      showDialog: false, // 控制弹层的显示和隐藏
+      depts: [], // 数据属性
       defaultProps: {
-        label: 'name', //要显示的字段的名字
-        children: 'children' //读取子节点的字段名
-      }//currentNoteId
+        label: 'name', // 要显示的字段的名字
+        children: 'children' // 读取子节点的字段名
+      }
     }
   },
   created() {
@@ -59,14 +59,33 @@ export default {
     // 封装好方法
     async getDepartment() {
       const result = await getDepartment()
-      this.depts = transListToTreeData(result,0)
+      this.depts = transListToTreeData(result, 0)
     },
-    //操作部门方法
-    operateDept(type,id) {
-      if(type === 'add') {
-        //添加子部门
-        this.showDialog = true //显示弹层
+    // 操作部门方法
+    operateDept(type, id) {
+      if (type === 'add') {
+        // 添加子部门
+        this.showDialog = true // 显示弹层
         this.currentNodeId = id
+      } else if (type === 'edit') {
+        // 编辑部门场景
+        this.showDialog = true
+        this.currentNodeId = id // 记录id 要用它获取数据
+        // 更新props- 异步动作
+        // 直接调用了子组件的方法 同步的方法
+        // 要在子组件获取数据
+        // 父组件调用子组件的方法来获取数据
+        this.$nextTick(() => {
+          this.$refs.addDept.getDepartmentDetail() // this.$refs.addDept等同于子组件的this
+        })
+      } else {
+        // 删除部门
+        this.$confirm('您确认要删除该部门吗').then(async() => {
+          await delDepartment(id)
+          // 提示消息
+          this.$message.success('删除部门成功')
+          this.getDepartment()
+        })
       }
     }
   }
@@ -81,11 +100,5 @@ export default {
   width: 50px;
   display: inline-block;
   margin: 10px;
-}
-el-dropdown-link {
-  margin-right: 40%;
-}
-.tree-manager {
-  margin-left: 20px;
 }
 </style>
